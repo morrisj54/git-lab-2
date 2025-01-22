40a376d73cbbc69165561ed4ae7acd70d7eeea6a
Jake Morris <jm289423@ohio.edu>
2025-01-21 20:22:05 -0500

    program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jake Morris
+ *        Date: 1/21/25
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
+//https://github.com/morrisj54
